78994b2fb07f4709a723954d3374842ccea62b9e
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-21 16:16:03 -0400

    Create a form for a new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  def help
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.expect(article: [ :title, :body ])
   end
 end
