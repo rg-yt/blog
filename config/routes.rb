4bd9624d00e75bd0273a7ca46803766b5754ccc7
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-21 15:43:33 -0400

    Create a mvc setup to show an article

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
   root "articles#index"
 end
