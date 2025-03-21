1721aca7be36b01319e1a8cdfd7e142b4c40828c
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-03-21 15:45:40 -0400

    Update routes to use resouces method

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
   root "articles#index"
+  resources :articles
 end
